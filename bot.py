0e575ffc2f62d47aa802292bcce77bfade39156c
murali2sa <murali.anusuri007@gmail.com>
2025-10-21 22:11:04 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+# bot_env.py
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"✅ Logged in as {self.user} (id: {self.user.id})")
+
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        if message.content.lower() == "!ping":
+            await message.channel.send("Pong!")
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
